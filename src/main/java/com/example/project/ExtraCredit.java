7bf1b1bfb7b5f14f3798cfa2bd58323e84d9d8fc
raffin44 <nashwannafi@gmail.com>
2024-09-26 21:39:41 +0000

    message

--- src/main/java/com/example/project/ExtraCredit.java
+++ src/main/java/com/example/project/ExtraCredit.java
@@ -1,4 +1,5 @@
 package com.example.project;
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class ExtraCredit {
@@ -7,20 +8,27 @@ public class ExtraCredit {
         StringBuilder result = new StringBuilder();
 
         // Copy and paste your existing tip calculator code here
-        double tipamt = (percent * cost) / 100;
-        double totalbill = (tipamt + cost); 
-        double perpersoncostbfr = (cost / people);
-        double perpersontip = (tipamt / people);
-        double totalcostperperson = (totalbill / people);
+        double tipamt = (percent*cost)/100;
+        double totalbill = (tipamt+cost);
+        double perpersoncostbfr = (cost/people);
+        double perpersontip = (tipamt/people);
+        double totalcostperperson = (totalbill/people);
+
+        DecimalFormat df = new DecimalFormat("0.00");
+        String roundedTipAmount = df.format(tipamt);
+        String roundedTotalBill = df.format(totalbill);
+        String roundedPerPersonCostBeforeTip = df.format(perpersoncostbfr);
+        String roundedPerPersonTip = df.format(perpersontip);
+        String roundedTotalCostPerPerson = df.format(totalcostperperson);
 
         result.append("-------------------------------\n");
-        result.append("Total bill before tip: $" + cost + "\n");
-        result.append("Total percentage: " + percent + "%" + "\n");
-        result.append("Total tip: $" + String.format("%.2f", tipamt) + "\n");
-        result.append("Total Bill with tip: $" + String.format("%.2f", totalbill) + "\n");
-        result.append("Per person cost before tip: $" + String.format("%.2f", perpersoncostbfr) + "\n");
-        result.append("Tip per person: $" + String.format("%.2f", perpersontip) + "\n");
-        result.append("Total cost per person: $" + String.format("%.2f", totalcostperperson) + "\n");
+        result.append("Total bill before tip: $"+ cost+"\n"); //concatenate to this string. DO NOT ERASE AND REWRITE
+        result.append("Total percentage: "+ percent+ "%" + "\n");
+        result.append("Total tip: $"+ roundedTipAmount+ "\n");
+        result.append("Total Bill with tip: $"+ roundedTotalBill+ "\n");
+        result.append("Per person cost before tip: $"+ roundedPerPersonCostBeforeTip+ "\n");
+        result.append("Tip per person: $"+ roundedPerPersonTip+"\n");
+        result.append("Total cost per person: $"+ roundedTotalCostPerPerson+"\n");
         result.append("-------------------------------\n");
 
         // Add the items ordered section
